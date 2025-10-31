14047b7ab68f31570a7e89c44e5df852b569a853
Joseph2205 <callisjoseph18@gmail.com>
2025-10-31 23:44:28 +0530

    Update and rename PRACTICE_1.java to T SECOND_SMASLLEST_LARGEST.java

--- T SECOND_SMASLLEST_LARGEST.java
+++ T SECOND_SMASLLEST_LARGEST.java
@@ -3,6 +3,9 @@ public class main{
         int []arr = {1,2,9,4,5,10,10};
         int min=Integer.MAX_VALUE;
         int sec_min=Integer.MAX_VALUE;
+        int max = 0;
+        int sec_max = 0;
+        
         for(int i=0;i<arr.length;i++){
             if(arr[i]<min){
                 sec_min = min;
@@ -11,8 +14,17 @@ public class main{
              else if (arr[i] < sec_min && arr[i] != min) {
                 sec_min = arr[i];
             }
+            if(arr[i]>max){
+                sec_max=max;
+                max = arr[i];
+            }
+            else if(arr[i]>sec_max && arr[i]!=max){
+                sec_max = arr[i];
+            }
         }
-        System.out.println(min);
+       // System.out.println(min);
          System.out.println(sec_min);
+         // System.out.println(max);
+         System.out.println(sec_max);
     }
 }
